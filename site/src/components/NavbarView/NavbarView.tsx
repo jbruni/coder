59efb58870a266f00cc47737eb8e7d9bb5bceda8
Garrett <garrett@coder.com>
2022-08-31 20:29:36 +0000

    fix infinity size

--- site/src/components/NavbarView/NavbarView.tsx
+++ site/src/components/NavbarView/NavbarView.tsx
@@ -101,7 +101,7 @@ export const NavbarView: React.FC<React.PropsWithChildren<NavbarViewProps>> = ({
 
       <Stack direction="row" className={styles.profileButton}>
         <div className={styles.quota} >
-          <WorkspaceQuota loading={false} count={1} limit={3}/>
+          <WorkspaceQuota loading={false} count={1} limit={0}/>
         </div>
 
           <div className={styles.profileButton}>
